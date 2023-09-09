2db0cc9f357df1586b8d5d6b5bdf7b07a3a59ba4
willotavio <willaotavio@gmail.com>
2023-09-09 19:59:26 -0300

    Add OptionMenu for letter case

--- functions.py
+++ functions.py
@@ -1,27 +1,33 @@
 import random
 
-characters = "abcdefghijklmnopqrstuv"
+characters = "abcdefghijklmnopqrstuvwxyz"
 numbers = "0123456789"
 symbols = "+-*/,.\\?\"';[]{}()_~^!@#$%&*|<>:`="
 
-def generate_password(length, options, characters_options):
+def generate_password(length, options):
     try:
         length = int(length)
         password = ""
         choices = []
 
         # choices array store the chosen options numbers like: choices = {1, 2} -> choices = (numbers and symbols)
-        c = 0
+        if options[0] != 'none':
+            choices.append(options[0])
+        c = 1
         while c < len(options):
             if options[c] == 1:
                 choices.append(c)
             c += 1
 
         if len(choices) > 0:
+            letter_case = ""
+            if choices[0] == 'lowercase' or choices[0] == 'uppercase' or choices[0] == 'lowercase/uppercase':
+                letter_case = choices[0]
+                choices[0] = 0
             for i in range(length):
-                char_type = random.randint(choices[0], choices[len(choices)-1])
+                char_type = random.randint(choices[0], choices[len(choices) - 1])
                 if char_type == 0:
-                    password += random_char(characters_options)
+                    password += random_char(letter_case)
                 elif char_type == 1:
                     password += random_number()
                 elif char_type == 2:
@@ -30,11 +36,11 @@ def generate_password(length, options, characters_options):
     except ValueError:
         return "Provide a valid length"
 
-def random_char(characters_options):
+def random_char(letter_case):
     char = characters[random.randint(0, len(characters) - 1)]
-    if characters_options[0] == 1 and characters_options[1] == 0:
+    if letter_case == 'lowercase':
         char = char.lower()
-    elif characters_options[0] == 0 and characters_options[1] == 1:
+    elif letter_case == 'uppercase':
         char = char.upper()
     else:
         if random.randint(0, 1) == 1:
--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 import tkinter as tk
 import functions as func
 
-def toggle_checkbox():
-    if characters.get() == 1:
-        lowercase_checkbox.config(state=tk.NORMAL)
-        uppercase_checkbox.config(state=tk.NORMAL)
-    else:
-        lowercase_checkbox.config(state=tk.DISABLED)
-        uppercase_checkbox.config(state=tk.DISABLED)
-
 window = tk.Tk()
 window.title("PassGen")
 window.geometry("420x420")
@@ -16,20 +8,17 @@ window.geometry("420x420")
 title = tk.Label(window, text="PassGen", font=("Arial", 15))
 title.pack()
 
-characters = tk.IntVar()
-lowercase = tk.IntVar()
-uppercase = tk.IntVar()
+options = ['lowercase', 'uppercase', 'lowercase/uppercase', 'none']
+
+clicked = tk.StringVar()
+clicked.set('lowercase/uppercase')
+
+chars = tk.OptionMenu(window, clicked, *options)
+
+chars.pack()
+
 numbers = tk.IntVar()
 symbols = tk.IntVar()
-
-characters_checkbox = tk.Checkbutton(window, text="Characters", variable=characters, onvalue=1, offvalue=0, command=toggle_checkbox)
-characters_checkbox.pack()
-
-lowercase_checkbox = tk.Checkbutton(window, text="Lowercase", variable=lowercase, onvalue=1, offvalue=0, state=tk.DISABLED)
-lowercase_checkbox.pack()
-
-uppercase_checkbox = tk.Checkbutton(window, text="Uppercase", variable=uppercase, onvalue=1, offvalue=0, state=tk.DISABLED)
-uppercase_checkbox.pack()
 
 numbers_checkbox = tk.Checkbutton(window, text="Numbers", variable=numbers, onvalue=1, offvalue=0)
 numbers_checkbox.pack()
@@ -41,7 +30,7 @@ length = tk.Entry(window)
 length.pack()
 submit = tk.Button(window, text="Submit", command=lambda: [
                                                     password.delete("1.0", tk.END),
-                                                    password.insert(tk.END, func.generate_password(length.get(), [characters.get(), numbers.get(), symbols.get()], [lowercase.get(), uppercase.get()]))
+                                                    password.insert(tk.END, func.generate_password(length.get(), [clicked.get(), numbers.get(), symbols.get()]))
                                                     ])
 submit.pack(pady=10)
 
